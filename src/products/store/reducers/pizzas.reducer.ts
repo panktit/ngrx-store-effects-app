29fe70bbd214fc1f0ffc466eadcafce0a7535763
panktit <panktit07@gmail.com>
2020-05-25 16:25:53 +0530

    :recycle:converting pizza details array to entities for:zap:performance boosting

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -3,13 +3,13 @@ import * as fromPizzas from '../actions/pizzas.actions';
 import { Pizza } from 'src/products/models/pizza.model';
 
 export interface PizzaState {
-    data: Pizza[];
+    entities: { [id: number] : Pizza };
     loaded: boolean;
     loading: boolean;
 }
 
 export const initialState: PizzaState = {
-    data: [],
+    entities: {},
     loaded: false,
     loading: false
 }
@@ -29,12 +29,24 @@ export function reducer (
         }
 
         case fromPizzas.LOAD_PIZZAS_SUCCESS: {
-            const data = action.payload;
+            const pizzas = action.payload;
+
+            // converting array to entities
+            const entities = pizzas.reduce((entities: { [id: number] : Pizza }, pizza: Pizza) => {
+                return {
+                    ...entities,
+                    [pizza.id]: pizza
+                };
+            }, 
+            {
+                ...state.entities
+            });
+
             return {
                 ...state, 
                 loading: false,
                 loaded: true,
-                data,
+                entities,
             };
         }
 
@@ -52,4 +64,4 @@ export function reducer (
 
 export const getPizzasLoading = (state: PizzaState) => state.loading;
 export const getPizzasLoaded = (state: PizzaState) => state.loaded;
-export const getPizzas = (state: PizzaState) => state.data;
+export const getPizzasEntities = (state: PizzaState) => state.entities;
