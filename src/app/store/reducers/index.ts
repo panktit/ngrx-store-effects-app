2c9e6f2cc0730aded82a1fbd373d698988f2648e
panktit <panktit07@gmail.com>
2020-05-25 17:13:13 +0530

    :alembic:adding custom router state serializer

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,6 +1,6 @@
 
 import * as fromRouter from '@ngrx/router-store';
-import { Params } from '@angular/router';
+import { Params, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 export interface RouterStateUrl {
@@ -21,3 +21,18 @@ export const reducers: ActionReducerMap<State> = {
 export const getRouterState = createFeatureSelector<
     fromRouter.RouterReducerState<RouterStateUrl>
 >('routerReducer');
+
+export class CustomSerializer implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+    serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+        const { url } = routerState;
+        const { queryParams } = routerState.root;
+
+        let state: ActivatedRouteSnapshot = routerState.root;
+        while(state.firstChild) {
+            state = state.firstChild;
+        }
+        const { params } = state;
+        
+        return { url, queryParams, params };
+    }
+}
