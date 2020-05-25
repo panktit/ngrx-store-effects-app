e8088aaa8dd01e1325f084df6ad61b85633299a8
panktit <panktit07@gmail.com>
2020-05-25 19:37:17 +0530

    :dizzy:added new @Effect to load toppings

--- src/products/store/effects/index.ts
+++ src/products/store/effects/index.ts
@@ -1,5 +1,7 @@
 import { PizzasEffects } from './pizzas.effects';
+import { ToppingsEffects } from './toppings.effect';
 
-export const effects: any[] = [PizzasEffects];
+export const effects: any[] = [PizzasEffects, ToppingsEffects];
 
 export * from './pizzas.effects';
+export * from './toppings.effect';
